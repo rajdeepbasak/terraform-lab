c2df72b27c58e0b305b4672688911225a160b8ba
Rajdeep Basak <rajdeep.tfg@gmail.com>
2021-11-09 12:43:47 +0530

    VM removed

--- main.tf
+++ main.tf
@@ -19,20 +19,3 @@ resource "google_compute_instance" "myvm" {
 		}
 	}
 }
-
-resource "google_compute_instance" "myvm2" {
-	name = "mysecondvm"
-	machine_type = "f1-micro"
-	zone = "us-central1-a"
-	network_interface {
-		network = "default"
-        access_config {
-          
-        }
-	}
-	boot_disk {
-		initialize_params {
-			image = "debian-cloud/debian-10"
-		}
-	}
-}
