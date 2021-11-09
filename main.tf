cd238263c1ee0f4b7172d10176bb88dd0e7d9a75
Rajdeep Basak <rajdeep.tfg@gmail.com>
2021-11-09 13:26:08 +0530

    variable added

--- main.tf
+++ main.tf
@@ -3,9 +3,14 @@ provider "google" {
 	region = "us-central1"
 }
 
+variable "mt" {
+    type = string
+    default = "fi-micro"
+}
+
 resource "google_compute_instance" "myvm" {
 	name = "myfirstvm"
-	machine_type = "f1-micro"
+	machine_type = var.mt
 	zone = "us-central1-a"
 	network_interface {
 		network = "default"
