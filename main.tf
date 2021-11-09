a00eb70bfdd0606711cb4dea8b75d536764af520
Rajdeep Basak <rajdeep.tfg@gmail.com>
2021-11-09 12:20:00 +0530

    File added

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+	project = "fresh-entity-315006"
+	region = "us-central1"
+}
+
+resource "google_compute_instance" "myvm" {
+	name = "myfirstvm"
+	machine_type = "f1-micro"
+	zone = "us-central1-a"
+	network_interface {
+		network = "default"
+        access_config {
+          
+        }
+	}
+	boot_disk {
+		initialize_params {
+			image = "debian-cloud/debian-10"
+		}
+	}
+}
